6af0546a32f6052b7823051178c4c4bdd84a3ccb
Will Boyd <whboyd@users.noreply.github.com>
2026-05-14 16:49:10 -0400

    vuln update

--- src/ledger/transactionhistory/src/main/java/anthos/samples/bankofanthos/transactionhistory/TransactionSearchService.java
+++ src/ledger/transactionhistory/src/main/java/anthos/samples/bankofanthos/transactionhistory/TransactionSearchService.java
@@ -1,33 +1,23 @@
-/*
- * Copyright 2020, Google LLC.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- */
-
 package anthos.samples.bankofanthos.transactionhistory;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TransactionSearchService {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Transactional(readOnly = true)
-    public List<?> searchByCounterparty(String accountId, String counterparty) {
+    public List<Map<String, Object>> searchByCounterparty(String accountId, String counterparty) {
         String sql = "SELECT transaction_id, from_acct, from_route, to_acct, to_route, amount, timestamp "
                 + "FROM transactions "
-                + "WHERE (from_acct = '" + accountId + "' OR to_acct = '" + accountId + "') "
-                + "AND (from_acct = '" + counterparty + "' OR to_acct = '" + counterparty + "')";
-        return entityManager.createNativeQuery(sql).getResultList();
+                + "WHERE from_acct = " + accountId;
+        return jdbcTemplate.queryForList(sql);
     }
 }
