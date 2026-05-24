81f90972c49e3bf0a9ae0c101f3632ebdd295f3b
Will Boyd <whboyd@users.noreply.github.com>
2026-05-23 21:30:33 -0400

    add detectable sqli

--- src/ledger/transactionhistory/src/main/java/anthos/samples/bankofanthos/transactionhistory/TransactionSearchService.java
+++ src/ledger/transactionhistory/src/main/java/anthos/samples/bankofanthos/transactionhistory/TransactionSearchService.java
@@ -21,4 +21,12 @@ public class TransactionSearchService {
                 + "OR (from_acct = '" + counterparty + "' AND to_acct = '" + accountId + "')";
         return jdbcTemplate.queryForList(sql);
     }
+
+    @Transactional(readOnly = true)
+    public List<Map<String, Object>> searchByAccountId(String accountId) {
+        String sql = "SELECT transaction_id, from_acct, from_route, to_acct, to_route, amount, timestamp "
+                + "FROM transactions "
+                + "WHERE from_acct = " + accountId;
+        return jdbcTemplate.queryForList(sql);
+    }
 }
